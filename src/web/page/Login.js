a7871445c00f13441ba527f34314dab4cddcd96c
yojic <dywlr74@naver.com>
2022-08-09 18:25:28 +0900

    네이버로 로그인 구현

--- src/web/page/Login.js
+++ src/web/page/Login.js
@@ -17,12 +17,13 @@ const Login = ()=>{
         callbackUrlNaver = "https://nsoohak.com/loginCallBackNaver";
     }
 
+    callbackUrlNaver = "http://localhost:3000/loginCallBackNaver";
+
     const naverLogin = new naver.LoginWithNaverId({
         clientId: "nHyzlpf4lzeLBMbSC5VL",
         callbackUrl: callbackUrlNaver, 
         isPopup: false, // popup 형식으로 띄울것인지 설정
-        loginButton: { color: 'white', type: 1, height: '47' }, //버튼의 스타일, 타입, 크기를 지정
-        
+        loginButton: { color: 'white', type: 3, height: '47' }, //버튼의 스타일, 타입, 크기를 지정
       });
 
     const initializeNaverLogin = () => {
@@ -32,60 +33,6 @@ const Login = ()=>{
     useEffect(() => {
         const asyncUseEffect = async () =>{
             initializeNaverLogin();
-            window.addEventListener('load', function () {
-                naverLogin.getLoginStatus(function (status) {
-                    console.log(naverLogin);
-                    if (status) {
-                        /* (5) 필수적으로 받아야하는 프로필 정보가 있다면 callback처리 시점에 체크 */
-                        console.log(naverLogin.user);
-                        var email = naverLogin.user.getEmail();
-                        var name = naverLogin.user.getName();
-                        var mobile = naverLogin.user.getMobile();
-                        var birthyear = naverLogin.user.getBirthyear();
-                        var birthday = naverLogin.user.getBirthday();
-                        if( email == undefined || email == null) {
-                            alert("이메일은 필수정보입니다. 정보제공을 동의해주세요.");
-                            /* (5-1) 사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함 */
-                            naverLogin.reprompt();
-                            return;
-                        }
-                        if( name == undefined || name == null) {
-                            alert("이름은 필수정보입니다. 정보제공을 동의해주세요.");
-                            /* (5-1) 사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함 */
-                            naverLogin.reprompt();
-                            return;
-                        }
-                        if( mobile == undefined || mobile == null) {
-                            alert("휴대폰번호는 필수정보입니다. 정보제공을 동의해주세요.");
-                            /* (5-1) 사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함 */
-                            naverLogin.reprompt();
-                            return;
-                        }
-                        if( birthyear == undefined || birthyear == null) {
-                            alert("생년월일은 필수정보입니다. 정보제공을 동의해주세요.");
-                            /* (5-1) 사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함 */
-                            naverLogin.reprompt();
-                            return;
-                        }
-                        if( birthday == undefined || birthday == null) {
-                            alert("생년월일은 필수정보입니다. 정보제공을 동의해주세요.");
-                            /* (5-1) 사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함 */
-                            naverLogin.reprompt();
-                            return;
-                        }
-                        
-                        console.log("네이버 로그인")
-                        var token = naverLogin.accessToken+"";
-                        var fake = token.split(".");
-                        var fakeToken= fake[1]+"";
-                        
-                    } else {
-                        console.log("callback 처리에 실패하였습니다.");
-                        alert("죄송합니다. 서버 오류로 로그인에 실패하였습니다. 다시 시도해주시기 바랍니다.\n지속적으로 로그인에 실패하실  주소창에 coksabu.com 을 입력하여 접속 후 다시 시도해주시기 바랍니다.");
-                         window.history.back();
-                    }
-                });
-            });
         }
 
         asyncUseEffect();
@@ -152,8 +99,11 @@ return (
                     <div className='login-input-div'><input id="password" name="password" className="login-input" type="password" placeholder='비밀번호를 입력해주세요.' onKeyUp={(event)=>fLoginUiVal(event)}/></div>
                     <div id="passChkValDesc" className='loginValDesc'></div>
                     <div id="loginErrMsg" className='loginErrMsg hide'></div>
-                    <div id="login-btn" className='login-btn disabled' onClick={(event)=>{fLoginBtnUiVal(event)}}>로그인</div> 
-                    <div className="grid-naver" id='naverIdLogin'></div>
+                    <div id="login-btn" className='login-btn disabled' onClick={(event)=>{fLoginBtnUiVal(event)}}>로그인</div>
+                    <div className="grid-naver hide" id='naverIdLogin'></div>
+                    <div className="naver-customize" onClick={()=>{naverLogin.init();window.location.href = naverLogin.generateAuthorizeUrl();}}>
+                        네이버 아이디로 로그인
+                    </div>
                     <div className='login-etc-info'>
                         <div><label><span id='checkCircle'></span><input onChange={()=>{fKeepLoginStateBtn()}} type="checkbox" id="emailSave" className='hide' />로그인 상태 유지하기</label></div>
                         <div id="emailPassFind"><a className="emailPassFind" href="#">아이디/비밀번호 찾기</a></div>
