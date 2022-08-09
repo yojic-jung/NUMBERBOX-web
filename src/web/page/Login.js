f5957026ff78206cf8359b64ee01d66eb9567ee0
yojic <dywlr74@naver.com>
2022-08-09 13:45:16 +0900

    네아로 임시

--- src/web/page/Login.js
+++ src/web/page/Login.js
@@ -19,7 +19,7 @@ const Login = ()=>{
 
     const naverLogin = new naver.LoginWithNaverId({
         clientId: "nHyzlpf4lzeLBMbSC5VL",
-        callbackUrl: "http://localhost:3000/loginCallBackNaver", 
+        callbackUrl: callbackUrlNaver, 
         isPopup: false, // popup 형식으로 띄울것인지 설정
         loginButton: { color: 'white', type: 1, height: '47' }, //버튼의 스타일, 타입, 크기를 지정
         
