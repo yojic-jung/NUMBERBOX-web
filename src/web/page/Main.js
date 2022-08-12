d3397c9b129b59bbeb2b44bc8f2f0030183cdbdf
yojic <dywlr74@naver.com>
2022-08-12 12:17:40 +0900

    메인화면 변경

--- src/web/page/Main.js
+++ src/web/page/Main.js
@@ -19,8 +19,65 @@ const Main = ()=>{
             nb_fadeInOutA("감사합니다. 회원가입이 정상적으로 완료 되었습니다.", 2000);
             window.history.pushState("", "N명의 수학", '/');
         }
+
+        const Confettiful = function(el) {
+            this.el = el;
+            this.containerEl = null;
+            
+            this.confettiFrequency = 3;
+            this.confettiColors = ['#EF2964', '#00C09D', '#2D87B0', '#48485E','#EFFF1D'];
+            this.confettiAnimations = ['slow', 'medium', 'fast'];
+            
+            this._setupElements();
+            this._renderConfetti();
+          };
+          
+          Confettiful.prototype._setupElements = function() {
+            const containerEl = document.createElement('div');
+            const elPosition = this.el.style.position;
+            
+            if (elPosition !== 'relative' || elPosition !== 'absolute') {
+              this.el.style.position = 'relative';
+            }
+            
+            containerEl.classList.add('confetti-container');
+            
+            this.el.appendChild(containerEl);
+            
+            this.containerEl = containerEl;
+          };
+          
+          Confettiful.prototype._renderConfetti = function() {
+            this.confettiInterval = setInterval(() => {
+              const confettiEl = document.createElement('div');
+              const confettiSize = (Math.floor(Math.random() * 3) + 7) + 'px';
+              const confettiBackground = this.confettiColors[Math.floor(Math.random() * this.confettiColors.length)];
+              const confettiLeft = (Math.floor(Math.random() * this.el.offsetWidth)) + 'px';
+              const confettiAnimation = this.confettiAnimations[Math.floor(Math.random() * this.confettiAnimations.length)];
+              
+              confettiEl.classList.add('confetti', 'confetti--animation-' + confettiAnimation);
+              confettiEl.style.left = confettiLeft;
+              confettiEl.style.width = confettiSize;
+              confettiEl.style.height = confettiSize;
+              confettiEl.style.backgroundColor = confettiBackground;
+              
+              confettiEl.removeTimeout = setTimeout(function() {
+                confettiEl.parentNode.removeChild(confettiEl);
+              }, 3000);
+              
+              this.containerEl.appendChild(confettiEl);
+            }, 25);
+          };
+          
+          window.confettiful = new Confettiful(document.querySelector('.js-container'));
+          
+          
+          
     })
 
+
+
+    
     
 
 return (
@@ -36,6 +93,17 @@ return (
             <div className='mainImgDesc second'>문제 공유와 손쉬운 학습지 제작</div>
             <div className='mainTitle'>N명의 사용자와 만들어가는,<br/>수학문제 공유 플랫폼</div>
         </div>
+        
+        <div className='relative'>
+            <div className="js-container container"></div>
+            <div className='js-container-inner'>
+                <div className='mainSubTitle1'>N명의 공유플랫폼, &nbsp;&nbsp;&nbsp;<span className='mainSubTitileName'>"N명<span className='mainSubTitileName2'>의</span>수학"</span></div>
+                <div className='mainSubDesc2'>
+                지금 N명의 수학을 누려보세요!
+                </div>
+            </div>  
+        </div>
+
         <div className='mainSubRootDiv1'>
             <div className='mainSubDiv1'>
                 <div>
@@ -56,15 +124,22 @@ return (
                 <div className='mainSubBox second'><img className='mainImgUI' src={mainSubConMake2} alt=""/></div>
             </div>
             <div className='mainSubDiv2Desc'>
-                <div className='mainSubDescTitle first'>수학 문제 제작시 번거로운 수식기호 입력을 쉽고 빠르게</div>
-                <div className='mainSubDescContents'>
-                    N명의 수학에서 제공하는 문제 제작 툴에서는 교육과정 및 교과서, 참고서에 나오는 기호들로만 구성하여 원하는 기호를 빠르게 찾을 수 있고, <br/>
-                    사용 빈도 수가 높은 기호를 단축키화하여 빠르게 입력할 수 있도록하여 문제 제작시 불필요한 소요시간을 줄일 수 있습니다.</div>
-                <div className='mainSubDescTitle second'>편집본까지 함께 제공되어 원본 문제를 수정하여 새로운 문제를 만들 수 있습니다.</div>
-                <div className='mainSubDescContents'>
-                    N명의 수학에서 제공하는 모든 문제는 완성된 원본 뿐만 아니라<br/>
-                    해설이 포함된 편집본까지 함께 제공되어 사용자가 수정하여 새로운 변형문제로 만들어 낼 수 있습니다.<br/>
+                <div>
+                    <div className='mainSubDescTitle first'>쉽고 빠른 수식입력 문제 제작 툴!</div>
+                    <div className='mainSubDescContents'>
+                        교육과정 및 교과서, 참고서에 나오는 기호들로만 구성하여 원하는 수식 기호를 빠르게 찾을 수 있고, <br/>
+                        수식 기호를 단축키화하여 빠르게 입력할 수 있도록하여 문제 제작시 불필요한 소요시간을 줄일 수 있습니다.
+                    </div>
                 </div>
+                <div>
+                    <div className='mainSubDescTitle second'>편집본 제공으로 변형문제 제작 가능</div>
+                    <div className='mainSubDescContents'>
+                        N명의 수학에서 제공하는 모든 문제는 완성된 원본 뿐만 아니라<br/>
+                        해설이 포함된 편집본까지 함께 제공되어 사용자가 수정하여 새로운 변형문제로 만들어 낼 수 있습니다.<br/>
+                    </div>
+                </div>
+
+
             </div>
         </div>
         <div className='mainSubDiv3'>
