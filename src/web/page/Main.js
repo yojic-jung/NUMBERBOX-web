c13cf46eaa1952a54184de1ebae17afb542e0f62
yojic <dywlr74@naver.com>
2022-08-16 17:45:43 +0900

    모바일 페이지 임시 구현

--- src/web/page/Main.js
+++ src/web/page/Main.js
@@ -151,20 +151,71 @@ return (
         </BrowserView>
         <MobileView>
             <div className='relative'>
-            <div className="js-container container mobile"></div>
-            <div className='js-container-inner mobile'>
-                <div className='mainSubTitle1'>
-                    N명의 공유플랫폼,<br/>
-                    <span className='mainSubTitileName'>"N명<span className='mainSubTitileName2'>의</span>수학"</span>
+                <div className="js-container container mobile"></div>
+                <div className='js-container-inner mobile'>
+                    <div className='mainSubTitle1'>
+                        N명의 공유플랫폼,<br/>
+                        <span className='mainSubTitileName'>"N명<span className='mainSubTitileName2'>의</span>수학"</span>
+                    </div>
+                    <div className='mainSubDesc2 mobile'>
+                    <span className='fontEmphasis'>[PC버전]</span>에서 서비스 제공중입니다.<br/>
+                    <span className='fontEmphasis'>수학 문제 만들기</span>, <span className='fontEmphasis'>문제 공유 서비스</span>,<br/>
+                    <span className='fontEmphasis'>학습지 제작</span>, <span className='fontEmphasis'>그래프 만들기</span>, <br/>
+                    <span className='fontEmphasis'>도형 파일 공유</span> 등의 서비스를 누려보세요!
+                    </div>
+                </div>  
+            </div>
+            <div className='mainSubRootDiv1 mobile'>
+                <div className='mainSubDiv1 mobile'>
+                    <div>
+                        <div className='mainSubTitle mobile'>손쉬운 학습지 제작</div>
+                        <div className='mainSubDesc mobile'>
+                            학년, 단원, 유형을 선택하여 난이도 및 문항 수에 맞게 사용자가 원하는
+                            형식과 문제로 학습지를 만들어 사용할 수 있습니다.
+                        </div>
+                        <div className='mainSubBox2 first'><img className='mainImgUI' src={main1} alt=""/></div>
+                    </div>
+                    <div className='mainSubBox first mobile'><img className='mainImgUI' src={mainSubDocsMake} alt=""/></div>
                 </div>
-                <div className='mainSubDesc2 mobile'>
-                <span className='fontEmphasis'>[PC버전]</span>에서 서비스 제공중입니다.<br/>
-                <span className='fontEmphasis'>수학문제 만들기</span>, <span className='fontEmphasis'>문제 공유 서비스</span>,<br/>
-                <span className='fontEmphasis'>학습지 제작</span>, <span className='fontEmphasis'>그래프 만들기</span>, <br/>
-                <span className='fontEmphasis'>도형 파일 공유</span> 등의 서비스를 누려보세요!
+            </div>
+            <div className='mainSubRootDiv2 mobile'>
+                <div className='mainSubDiv2Title mobile'>나의 제작 문제 및 변형 문제 만들기</div>
+                <div className='mainSubDiv2 mobile'>
+                    <div className='mainSubBox first'><img className='mainImgUI' src={mainSubConMake} alt=""/></div>
+                    <div className='mainSubBox second'><img className='mainImgUI' src={mainSubConMake2} alt=""/></div>
                 </div>
-            </div>  
-        </div>
+                <div className='mainSubDiv2Desc mobile'>
+                    <div>
+                        <div className='mainSubDescTitle first mobile'>쉽고 빠른 수식입력 문제 제작 툴!</div>
+                        <div className='mainSubDescContents mobile'>
+                            교육과정 및 교과서, 참고서에 나오는 기호들로만 구성하여 원하는 수식 기호를 빠르게 찾을 수 있고, <br/>
+                            수식 기호를 단축키화하여 빠르게 입력할 수 있도록하여 문제 제작시 불필요한 소요시간을 줄일 수 있습니다.
+                        </div>
+                    </div>
+                    <div>
+                        <div className='mainSubDescTitle second mobile'>편집본 제공으로 변형문제 제작 가능</div>
+                        <div className='mainSubDescContents mobile'>
+                            N명의 수학에서 제공하는 모든 문제는 완성된 원본 뿐만 아니라
+                            해설이 포함된 편집본까지 함께 제공되어 사용자가 수정하여 새로운 변형문제로 만들어 낼 수 있습니다.<br/>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+            <div className='mainSubDiv3 mobile'>
+                <div>
+                    <div className='mainSubTitle mobile'>도형 및 그래프 공유(그래프 제작 툴)</div>
+                    <div className='mainSubDesc'>
+                    수학문제 제작시 많은 시간이 소요되는 이미지나 그래프 등을 사용자들과 공유하여 사용할 수 있습니다.<br/>
+                    또한, 그래프 제작 툴을 제공하여 사용자들이 쉽게 그래프를 만들 수 있습니다.
+                    </div>
+                </div>
+                <div className='relative alignRight'>
+                    <div className='mainSubBox first'><img className='mainImgUI' src={main3} alt=""/></div>
+                    <div className='mainSubBox second'><img className='mainImgUI' src={mainResourceMake} alt=""/></div>
+                </div>
+            </div>
         </MobileView>
     </>
     )
