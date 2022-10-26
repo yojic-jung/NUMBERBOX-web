d864a0bd2628a48a86c97e5908f757f27c08eed2
yojic <dywlr74@naver.com>
2022-10-26 16:20:28 +0900

    react-snap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { hydrate, render } from "react-dom";
+import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<HelmetProvider><App /></HelmetProvider>, rootElement);
-} else {
-  render(<HelmetProvider><App /></HelmetProvider>, rootElement);
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <HelmetProvider>
+    <App />
+    </HelmetProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-
-
-
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
